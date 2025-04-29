7082558226227ec7b183cc9ae7cce87229822f26
justmahmoud31 <justmahmoud3152003@gmail.com>
2025-04-29 20:14:28 +0300

    Enhanced : Protected Routes

--- src/Components/ProtectedRoutes.jsx
+++ src/Components/ProtectedRoutes.jsx
@@ -2,7 +2,7 @@ import { useSelector } from "react-redux";
 import { Navigate } from "react-router-dom";
 
 function PrivateRoute({ children }) {
-    const { isAuthenticated } = useSelector((state) => state.auth);
+    const { isAuthenticated } = !!localStorage.getItem("token");
 
     return isAuthenticated ? children : <Navigate to="/login" />;
 }
